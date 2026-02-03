cd7a56a9ce707d68a9bb80fc3c1a3d80b2614b4e
Felix Octaniel <felixoctanieltel05@gmail.com>
2026-02-03 22:22:05 +0700

    update deploy dumy

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -1,3 +1,5 @@
+import { Message } from "../types/chat"
+
 export const DUMMY_CONTACTS = [
   { id: 1, name: "AI Business Advisor", lastMsg: "Strategi scaling bisnis kamu bisa kita optimasi.", time: "09:12", online: true },
   { id: 2, name: "Rina Customer Support", lastMsg: "Halo kak, ada yang bisa dibantu?", time: "08:45", online: true },
@@ -6,22 +8,24 @@ export const DUMMY_CONTACTS = [
   { id: 5, name: "Ayunda Marketing", lastMsg: "Campaign minggu depan kita push ya", time: "Senin", online: true },
 ];
 
-export const DUMMY_MESSAGES = {
+
+
+export const DUMMY_MESSAGES: Record<number, Message[]> = {
   1: [
-    { sender: "bot", text: "Halo Felix! Mau diskusi bisnis apa hari ini?" },
-    { sender: "user", text: "Gimana cara scale startup lebih cepat?" },
+    { id: 1, sender: "bot", text: "Halo Felix! Mau diskusi bisnis apa hari ini?" },
+    { id: 2, sender: "user", text: "Gimana cara scale startup lebih cepat?" },
   ],
   2: [
-    { sender: "bot", text: "Halo kak, ada kendala?" },
+    { id: 1, sender: "bot", text: "Halo kak, ada kendala?" },
   ],
   3: [
-    { sender: "bot", text: "Wireframe dashboard udah aku share ya." },
-    { sender: "user", text: "Siap, nanti aku cek." },
+    { id: 1, sender: "bot", text: "Wireframe dashboard udah aku share ya." },
+    { id: 2, sender: "user", text: "Siap, nanti aku cek." },
   ],
   4: [
-    { sender: "bot", text: "Endpoint auth sudah aman." },
+    { id: 1, sender: "bot", text: "Endpoint auth sudah aman." },
   ],
   5: [
-    { sender: "bot", text: "Campaign ads mulai kapan?" },
+    { id: 1, sender: "bot", text: "Campaign ads mulai kapan?" },
   ],
-};
+}
